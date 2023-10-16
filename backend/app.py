43a7e815985493f2581d5c02d12876d10343632f
SergioRon2 <rondonjr2005@gmail.com>
2023-10-16 17:47:05 -0500

    16/10/2023

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template
 from flask import request, jsonify
 from flask_cors import CORS
-import pymysql
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('Frontend/index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
